dbbc78cc6d0d24897c5f492a523832d800ed61a8
Dan Lages <danlages@users.noreply.github.com>
2018-03-30 23:28:11 +0100

    Implementation of defineImages function
    Implementation of defineImages function to load images ready for pre-processing

--- PreProcessing.py
+++ PreProcessing.py
@@ -1,44 +1,65 @@
-from skimage import data, io, filters, threshold_otsu,
+from skimage import data, io, filters
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 import numpy as np
+import glob
+import shutil
+from PIL import Image
+
 import threading, os, glob
 
 
 #Create array of charaters in folder
 
 def defineImages(charactersPath):
-#Will need to crop here - (mnist supports 20*20)
-    for image in charactersPath:   #loop through folder destination
-
-return characterArray
-
-def thresholding(characterFile):
-
-    for image in characterArray
-
-return characterArray
-
-def noiseReduction(characterArray):
-
-    for image in characterArray
-
-return characterArray
-
-def shapness(characterArray):
-
-    for image in characterArray
-
-return characterArray
+#Will need to crop here - (mnist supports 20*20
+    directoryList = os.listdir(charactersPath)
+    characterArray = []
+    for file in directoryList:   #loop through folder destination
+        if file.endswith(".jpeg") or file.endswith(".jpg"):
+            img = Image.open(os.path.join(charactersPath, file))
+            #numpyArray = np.array(img)
+            characterArray.append(img)
+    return characterArray
 
 
-def saveToFolder(characterArray):
+#def convertToArray:
 
-    for image in characterArray
+#def thresholding(characterArray):
+    #for image in characterArray:
+
+    #return characterArray
+
+#def noiseReduction(characterArray):
+#    for image in characterArray:
+
+#    return characterArray
+
+#def shapness(characterArray):
+    #for image in characterArray:
+
+    #return characterArray
+
+
+#def saveToFolder(characterArray, destination):
+    #for file in characterArray:
+
+    #return characterArray
 
 
 if __name__ == "__main__":
 
-charactersPath = "/Users/daniellages/Documents/Computer Science/Year 3/OCR/Implementation/inputImages"
+    charactersPath = "/Users/daniellages/Documents/Computer Science/Year 3/OCR/Implementation/inputImages"  #Destination of images to process
+    destinationPath = "/Users/daniellages/Documents/Computer Science/Year 3/OCR/Implementation/processed"   #Desitnation to save processed images
 
-arrayOfCharacters = defineImages(charactersPath)
+    arrayOfCharacters = defineImages(charactersPath)
+
+    #for image in arrayOfCharacters:
+        #Image.show(image)
+
+    for file in arrayOfCharacters:
+        file.show()
+
+    #saveToFolder(arrayOfCharacters, destinationPath)
+
+    #print arrayOfCharacters
