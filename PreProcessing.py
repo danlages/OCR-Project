ca4427717596d7df6e67e090c366f0a15b95b13e
Dan Lages <danlages@users.noreply.github.com>
2018-04-03 21:44:11 +0100

    Pre-Processing Additions
    Implementation of thresholding and noise reduction functions

--- PreProcessing.py
+++ PreProcessing.py
@@ -1,65 +1,60 @@
-from skimage import data, io, filters
+from skimage import data, io, filters, exposure
+from skimage.exposure import equalize_hist
+from skimage.color import rgb2gray
+from skimage.filters import threshold_mean, median, rank
+from skimage.morphology import disk
 import matplotlib.pyplot as plt
-import matplotlib.cm as cm
 import numpy as np
-import glob
-import shutil
+
 from PIL import Image
+from scipy import misc, ndimage
 
 import threading, os, glob
 
+SIZE = 20, 20
 
 #Create array of charaters in folder
 
-def defineImages(charactersPath):
-#Will need to crop here - (mnist supports 20*20
-    directoryList = os.listdir(charactersPath)
+def noiseReduction(image):
+    image = filters.median(image, np.ones((3 , 3)))
+    noise = np.random.random(image.shape)
+    image[noise > 0.99] = 255
+    image[noise < 0.01] = 0
+    image = median(image, disk(1))
+    return image
+
+def thresholding(image):
+    thresh = threshold_mean(image)
+    image = image > thresh
+    return image
+
+
+def modify(path):
+    #function for running functions
     characterArray = []
+    directoryList = os.listdir(path)
     for file in directoryList:   #loop through folder destination
         if file.endswith(".jpeg") or file.endswith(".jpg"):
-            img = Image.open(os.path.join(charactersPath, file))
-            #numpyArray = np.array(img)
-            characterArray.append(img)
+            img = os.path.join(charactersPath, file)
+            image = io.imread(img)
+            image = rgb2gray(image)
+            image = noiseReduction(image)
+            image = thresholding(image)
+            characterArray.append(image) #can append to array to be used in other functions :D
+            io.imshow(image)
+            io.show()
     return characterArray
-
-
-#def convertToArray:
-
-#def thresholding(characterArray):
-    #for image in characterArray:
-
-    #return characterArray
-
-#def noiseReduction(characterArray):
-#    for image in characterArray:
-
-#    return characterArray
-
-#def shapness(characterArray):
-    #for image in characterArray:
-
-    #return characterArray
-
-
-#def saveToFolder(characterArray, destination):
-    #for file in characterArray:
-
-    #return characterArray
 
 
 if __name__ == "__main__":
 
-    charactersPath = "/Users/daniellages/Documents/Computer Science/Year 3/OCR/Implementation/inputImages"  #Destination of images to process
+    charactersPath = "/Users/daniellages/Documents/Computer Science/Year 3/OCR/OCR-Project/inputImages"  #Destination of images to process
     destinationPath = "/Users/daniellages/Documents/Computer Science/Year 3/OCR/Implementation/processed"   #Desitnation to save processed images
 
-    arrayOfCharacters = defineImages(charactersPath)
+    arrayOfCharacters = []
+    arrayOfCharacters = modify(charactersPath)
 
-    #for image in arrayOfCharacters:
-        #Image.show(image)
-
-    for file in arrayOfCharacters:
-        file.show()
-
-    #saveToFolder(arrayOfCharacters, destinationPath)
-
-    #print arrayOfCharacters
+"""    for image in arrayOfCharacters:
+        io.imshow(image)
+        io.show()
+        io.imsave('fileTest.png', image)"""
